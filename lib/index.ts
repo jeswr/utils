cc90d20577654badcc35dfbf5a075fef7b458741
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2024-06-04 23:11:13 +0000

    feat: initial features (#3)
    * feat: initial features
    
    * chore: update node versions

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,2 @@
-export default {};
+export * from './getSubjects';
+export * from './shapeFromDataset';
---
+++ lib/shapeFromDataset.ts
@@ -0,0 +1,43 @@
+import { LdoBase, ShapeType, createLdoDataset } from '@ldo/ldo';
+import { DatasetCore, NamedNode, BlankNode } from '@rdfjs/types';
+import RdfJsDb from '@shexjs/neighborhood-rdfjs';
+import { ShExValidator } from '@shexjs/validator';
+import dereferenceToStore from 'rdf-dereference-store';
+import { getSubjects } from './getSubjects';
+
+export function shapeFromDataset<T extends LdoBase>(
+  shapeType: ShapeType<T>,
+  dataset: DatasetCore,
+  subject: NamedNode | BlankNode | string,
+): T {
+  const validator = new ShExValidator(shapeType.schema, (RdfJsDb as any).ctor(dataset));
+  const validationResult = validator.validateShapeMap([{
+    node: typeof subject === 'string' ? subject : subject.value,
+    shape: shapeType.shape,
+  }]);
+  if (validationResult[0].status !== 'conformant') {
+    throw new Error(JSON.stringify(validationResult, null, 2));
+  }
+  return createLdoDataset([...dataset]).usingType(shapeType).fromSubject(subject);
+}
+
+export async function dereferenceShape<T extends LdoBase>(
+  shapeType: ShapeType<T>,
+  source: string,
+  // istanbul ignore next
+  // eslint-disable-next-line default-param-last
+  subject: NamedNode | BlankNode | string = source,
+  options?: Parameters<typeof dereferenceToStore>[1],
+) {
+  return shapeFromDataset(shapeType, (await dereferenceToStore(source, options)).store, subject);
+}
+
+export function* shapeMatches<T extends LdoBase>(shapeType: ShapeType<T>, dataset: DatasetCore) {
+  for (const subject of getSubjects(dataset)) {
+    try {
+      yield shapeFromDataset(shapeType, dataset, subject);
+    } catch (e) {
+      // Do nothing, not all subjects will match every shape
+    }
+  }
+}
